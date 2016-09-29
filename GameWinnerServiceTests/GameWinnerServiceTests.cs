de8e6c41bdc0f98b76c743f743ee10563a517874
Ortega <Ortega@DESKTOP-2PFGEBL>
2016-09-29 10:47:13 +0200

    Add project files.

---
+++ GameWinnerServiceTests/GameWinnerServiceTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe.Services;
+
+namespace TicTacToeTests
+{
+    [TestClass]
+    public class GameWinnerServiceTests
+    {
+
+        IGameWinnerService _gameWinnerService;
+        private char[,] _gameBoard;
+
+        [TestInitialize]
+        public void SetupUnitTests()
+        {
+            _gameWinnerService = new GameWinnerService();
+            _gameBoard = new char[3, 3]{
+                                      {' ', ' ', ' '},
+                                      {' ', ' ', ' '},
+                                      {' ', ' ', ' '},
+            };
+        }
+
+        [TestMethod]
+        public void NeitherPlayerHasThreeInARow()
+        {
+            const char expected = ' ';
+            var gameBoard = new char[3, 3] { {' ', ' ', ' '},
+                                             {' ', ' ', ' '},
+                                             {' ', ' ', ' '}};
+            var actual = _gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void PlayerWithAllSpacesInTopRowIsWinner()
+        {
+            const char expected = 'X';
+            var gameBoard = new char[3, 3]{ 
+                                            {expected, expected, expected},
+                                            {' ', ' ', ' '},
+                                            {' ', ' ', ' '} };
+            var actual = _gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [TestMethod]
+        public void PlayerWithAllSpacesInBottomRowIsWinner()
+        {
+            const char expected = 'X';
+            var gameBoard = new char[3, 3]{
+                                            {' ', ' ', ' '},
+                                            {' ', ' ', ' '},
+                                            {expected, expected, expected} };
+            var actual = _gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [TestMethod]
+        public void PlayerWithAllSpacesInMiddleRowIsWinner()
+        {
+            const char expected = 'X';
+            var gameBoard = new char[3, 3]{
+                                            {' ', ' ', ' '},
+                                            {expected, expected, expected},
+                                            {' ', ' ', ' '} };
+            var actual = _gameWinnerService.Validate(gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [TestMethod]
+        public void PlayerWithAllSpacesInFirstColumnIsWinner()
+        {
+            const char expected = 'X';
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                _gameBoard[columnIndex, 0] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [TestMethod]
+        public void PlayerWithAllSpacesInThirdColumnIsWinner()
+        {
+            const char expected = 'X';
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                _gameBoard[columnIndex, 2] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [TestMethod]
+        public void PlayerWithAllSpacesInSecondColumnIsWinner()
+        {
+            const char expected = 'X';
+            for (var columnIndex = 0; columnIndex < 3; columnIndex++)
+            {
+                _gameBoard[columnIndex, 1] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [TestMethod]
+        public void PlayerWithThreeInARowDiagonallyDownAndToRightIsWinner()
+        {
+            const char expected = 'X';
+            for (var cellIndex = 0; cellIndex < 3; cellIndex++)
+            {
+                _gameBoard[cellIndex, cellIndex] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+        [TestMethod]
+        public void PlayerWithThreeInARowDiagonallyTopAndToLeftIsWinner()
+        {
+            const char expected = 'X';
+            for (var cellIndex = 0; cellIndex < 3; cellIndex++)
+            {
+                _gameBoard[cellIndex, (2 - cellIndex)] = expected;
+            }
+            var actual = _gameWinnerService.Validate(_gameBoard);
+            Assert.AreEqual(expected.ToString(), actual.ToString());
+        }
+    }
+}
+
---
+++ TicTacToe.Services/GameWinnerService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Services
+{
+    public interface IGameWinnerService
+    {
+        char Validate(char[,] gameBoard);
+    }
+
+    public class GameWinnerService : IGameWinnerService
+    {
+        private const char SYMBOL_FOR_NO_WINNER = ' ';
+        private const char SYMBOL = 'X';
+        public char Validate(char[,] gameBoard)
+        {
+            var currentWinningSymbol = CheckForThreeInARowInHorizontalRow(gameBoard);
+            if (currentWinningSymbol != SYMBOL_FOR_NO_WINNER)
+            {
+                return currentWinningSymbol;
+            }
+            currentWinningSymbol = CheckForThreeInARowInVerticalColumn(gameBoard);
+            if (currentWinningSymbol != SYMBOL_FOR_NO_WINNER)
+            {
+                return currentWinningSymbol;
+            }
+            currentWinningSymbol = CheckForThreeInARowDiagonally(gameBoard);
+            return currentWinningSymbol;
+        }
+        private static char CheckForThreeInARowInHorizontalRow(char[,] gameBoard)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                var columnOneChar = gameBoard[i, 0];
+                var columnTwoChar = gameBoard[i, 1];
+                var columnThreeChar = gameBoard[i, 2];
+                if (columnOneChar == SYMBOL &&
+                    columnTwoChar == SYMBOL &&
+                    columnThreeChar == SYMBOL)
+                {
+                    return columnOneChar;
+                }
+            }
+            return SYMBOL_FOR_NO_WINNER;
+        }
+        private static char CheckForThreeInARowInVerticalColumn(char[,] gameBoard)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                var rowOneChar = gameBoard[0, i];
+                var rowTwoChar = gameBoard[1, i];
+                var rowThreeChar = gameBoard[2, i];
+                if (rowOneChar == SYMBOL &&
+                    rowTwoChar == SYMBOL &&
+                    rowThreeChar == SYMBOL)
+                {
+                    return rowOneChar;
+                }
+            }
+            return SYMBOL_FOR_NO_WINNER;
+        }
+        private static char CheckForThreeInARowDiagonally(char[,] gameBoard)
+        {
+            var cellTwoChar = gameBoard[1, 1];
+            int a = 0;
+            for (int i = 0; i < 2; i++)
+            {
+                var cellOneChar = gameBoard[0, a];
+                if (i == 0) a += 2;
+                else a -= 2;
+                var cellThreeChar = gameBoard[2, a];
+                if (cellOneChar == cellTwoChar &&
+                cellTwoChar == cellThreeChar)
+                {
+                    return cellOneChar;
+                }
+            }
+            return SYMBOL_FOR_NO_WINNER;
+        }
+    }
+}
+
